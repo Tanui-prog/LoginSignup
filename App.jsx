9b4da1c8b0399a92534530ea08fdb77d720a389e
Tanui <vikiikiptoo@gmail.com>
2024-05-24 11:45:47 +0300

    new file:   App.jsx 	deleted:    App.tsx 	modified:   package-lock.json 	modified:   package.json

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+const App = () => {
+  return (
+
+    <NavigationContainer>{
+      <View>
+      <Text>App</Text>
+    </View>
+  
+      }</NavigationContainer>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
